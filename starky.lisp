c2f72f3cbced2d21d310d7ece544ee61efc525a8
stack <fpgasm@apple2.x10.mx>
2017-08-30 07:10:32 +0000

    ok

--- starky.lisp
+++ starky.lisp
@@ -50,7 +50,7 @@
 (defun text (x y string font pointsize)
   (let ((old-matrix (foreign-alloc :float :count 9))
 	(matrix (foreign-alloc :float :initial-contents
-			       (list pointsize 0.0 0.0
+			       (list  pointsize 0.0 0.0
 				     0.0 pointsize 0.0
 				     0.0 0.0 1.0))))
     (vg:get-matrix old-matrix)
@@ -62,13 +62,13 @@
     (loop for c across string
        for glyph-index =  (aref  (font-character-map font) (char-code c))
        for xx = x then (+ xx
-			  (/ (* pointsize
+			  (/ (* pointsize 
 				(aref (font-glyph-advances font) glyph-index))
 			     65536.0))
        unless (= -1 glyph-index)
        do
 	 (setf (mem-ref matrix :float 24) xx
-	       (mem-ref matrix :float 28) y)
+	       (mem-ref matrix :float 28) y  )
 	 (vg:load-matrix old-matrix)
 	 (format t "~&B. ~A ~A ~X" xx y( vg:get-error ))
 	 (vg:mult-matrix matrix)
@@ -158,13 +158,16 @@
 (defun work ()
  ;; (declare (optimize (speed 3) (safety 0) (debug 0)))
   (with:all
-      ((vec (rgb-back '(0.0 0.0 0.0 1.0)		;'(0.9 0.2 0.3 1.0)
+      ((vec (rgb-back '(1.0 1.0 1.0 1.0)		;'(0.9 0.2 0.3 1.0)
 	     ))
-       (vec (rgb-fill '(1.0 0.7 1.0 1.0))))
+       (vec (rgb-fill '(0.0 0.0 0.0 1.0))))
     (background rgb-back)
     (set-fill rgb-fill)
+    (vg:set-i vg:rendering-quality vg:quality-faster
+	      )
     (circle 500.0 0.0 500.0  )
-    (text 100.0 500.0 "The qiuick brown fox jumps over the lazy fox" *font* 7.9)
+   ;; (stroke-width 5.0)
+    (text 100.0 500.0 "The qiuick brown fox jumps over the lazy fox" *font* 19.0)
 
 
 ))
@@ -172,19 +175,27 @@
 (defun tin ()
   ( native::init :api egl:openvg-api)
   (load-font *cold-font*)
+    (vg:set-i vg:rendering-quality vg:quality-better)
+    (vg:set-i vg:pixel-layout vg:pixel-layout-rgb-horizontal)
+    (vg:set-i vg:screen-layout vg:pixel-layout-rgb-horizontal)
+    
 )
 (defun ttt ()
   ;; background
   (work)
-   (print (vg:get-error)) (force-output)
+  (print (vg:get-error)) (force-output)
+  (vg:set-i vg:rendering-quality vg:quality-better
+	    )
   (vg:flush)
   (egl:swap-buffers native::*surface*)
   ;;(sleep 10)
+  ;;(egl:swap-buffers native::*surface*)
   
   )
 (defun tout ()
   (unload-font *font*)
-  (native::deinit))
+  (native::deinit)
+)
 
 
 
@@ -204,3 +215,20 @@
 
     (loop for iy from y to (+ y h) by n
 	 do (line x iy (+ x w) iy))))
+
+
+(defun fuckle (w)
+  (with:all
+      ((dx:rect (dst-rect 0 0 960 540
+					;(+ w 100) 100 920 580
+		 ))
+       (dx:rect (src-rect 0 0 (ash 1920 16) (ash 1080 16))))
+    (let ((update (dx:update-start))
+	  (element (mem-ref native::*native* :uint)))
+      (dx:element-change-attributes
+       update element dx:CHANGE-TRANSFORM 0 255 dst-rect src-rect  0 :transform #x20000)
+	(dx:update-submit-sync update)))
+  )
+
+;;(loop for i from 10 to 134 by 1 do (fuckle i))
+
