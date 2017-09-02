450d5d6f88956a16cda6d13a5f24bfc0d43d6b39
stack <fpgasm@apple2.x10.mx>
2017-09-02 00:26:02 +0000

    rusty, but working ft2 font handling

--- ft.lisp
+++ ft.lisp
@@ -6,11 +6,64 @@
       (ft2:new-face "/usr/share/fonts/truetype/dejavu/DejaVuSansMono.ttf")))
 
 (ft2:set-char-size *face* (* 11 64)
-		   (* 11 64)  72 72)
+		   (* 12 64)  72 72)
 
+;; cache of codes we have
 (defparameter *cache* (make-hash-table))
 
+(defparameter *vgfont* (vg:create-font 0))
+(defparameter *origin* (make-vec '(0.0 0.0)))
+(defun glyph-add (char bm advance left top)
+ (declare (optimize (speed 0) (safety 3) (debug 3)))
+  
+ (let* ((code (char-code char))
+	(w    (ft2::ft-bitmap-width  bm))
+	(h    (ft2::ft-bitmap-rows   bm))
+	(pitch  (ft2::ft-bitmap-pitch  bm))
+	(buffer (ft2::ft-bitmap-buffer bm)))
+   (format t "~A ~A ~A ~A~&" char advance left top)
+   (if (zerop h)
+       (let ((glyph-index (ft2:get-char-index *face* char)))
+;;	 (format t "char ~A index ~A code ~A~&" char glyph-index code)
+	 (with-vec (escapement '(7.0 0.0 ))
+	   (with-vec (origin (list 0.0 (float (- h top))))
+	     (vg:set-glyph-to-image
+	      *vgfont* code
+		 0 origin escapement))))
+       (vg:with-image (vg-image
+		       vg:a-8 w h
+		       vg:image-quality-nonantialiased)
+	 (vg:image-sub-data vg-image
+			    (cffi-sys:inc-pointer  buffer (* pitch  (-  h 1))) ; last line
+			    (- 0 pitch)
+			    vg:a-8
+			    0 0 w h)
+	 (let ((glyph-index (ft2:get-char-index *face* char)))
+;;	   (format t "char ~A index ~A code ~A~&" char glyph-index code)
+	   (with-vec (escapement '(7.0 0.0 ))
+	     (with-vec (origin (list (float (-  left)) (float (- h top))))
+	       (vg:set-glyph-to-image
+		*vgfont* code
+		vg-image origin escapement))))))))
 
+  
+(defun glyph-assure (face char)
+  "Assure that the glyph for this character exists. Return vg glyph id"
+  (unless (gethash char *cache*)
+    (setf (gethash char *cache*) t)
+    (ft2:load-char face char)
+    (let ((glyphslot (ft2::render-glyph face)))
+      (glyph-add char
+		 (ft2::ft-glyphslot-bitmap glyphslot)
+		 (ft2::get-loaded-advance face nil)
+		 (ft2::ft-glyphslot-bitmap-left glyphslot)
+		 (ft2::ft-glyphslot-bitmap-top glyphslot)))))
+
+
+(defun my-print (string mode)
+  (loop for char across string do
+       (glyph-assure *face* char)
+       (vg:draw-glyph *vgfont* (char-code char) mode 0 )))
 ;;-----------------------------------------------------------------------------
 
 
@@ -23,15 +76,20 @@ Custom functions must be compatible, though any (non-`NIL`) value may
 be returned in the place of `BITMAP`.  Note that cl-freetype2 does nothing
 else for you.  If you want your cache populated, you must do this yourself,
 for instance, within the [`DO-STRING-RENDER`](#DO-STRING-RENDER) loop."
-    (let ((glyphslot (ft2:render-glyph face ;;:lcd
-				     )))
-  ;;  (ft2:bitmap-convert glypslot  )
+  (ft2:load-char face char (if vertical-p '(:vertical-layout) '(:default)))
+  (let ((glyphslot (ft2::render-glyph face)))
+    (glyph-add char
+	       (ft2::ft-glyphslot-bitmap glyphslot)
+	       (ft2::get-loaded-advance face vertical-p)
+	       (ft2::ft-glyphslot-bitmap-left glyphslot)
+	       (ft2::ft-glyphslot-bitmap-top glyphslot))
     (values (ft2::ft-glyphslot-bitmap glyphslot)
 	    (ft2::get-loaded-advance face vertical-p)
-            (ft2::ft-glyphslot-bitmap-left glyphslot)
-	    (ft2::ft-glyphslot-bitmap-top glyphslot))))
+	    (ft2::ft-glyphslot-bitmap-left glyphslot)
+            (ft2::ft-glyphslot-bitmap-top glyphslot))))
 
-(defun cuck ()
+
+(defun cuckx ()
    
   (let ((image (vg:create-image
 		vg:s-l-8 700 30
@@ -76,7 +134,7 @@ for instance, within the [`DO-STRING-RENDER`](#DO-STRING-RENDER) loop."
 	    (pitch  (ft2::ft-bitmap-pitch  bm))
 	    (buffer (ft2::ft-bitmap-buffer bm)))
 	(unless (zerop w)
-	  (format t "~& ~A "  (ft2::ft-bitmap-pixel-mode bm))
+;;	  (format t "~& ~A "  (ft2::ft-bitmap-pixel-mode bm))
 
 	  (vg:image-sub-data image
 ;;			     buffer
@@ -91,16 +149,28 @@ for instance, within the [`DO-STRING-RENDER`](#DO-STRING-RENDER) loop."
     ;;(vg:draw-image image)
     (vg:set-pixels 100 470 image 0  0  700 300)	;
     (vg:destroy-image image)
+
+    
+    (with-vec (origin '(100.0 520.0))
+      (vg:set-fv vg:glyph-origin 2 origin))
+    (vg:set-i vg:image-mode vg:draw-image-stencil)
+    (my-print "The xxx \\ quick brown fox Jumps over the lazy dog"  vg:fill-path  )
+;;    (vg:draw-glyph *vgfont* (char-code #\g) (+ vg:stroke-path vg:fill-path) 0 )
+
+    (print (vg:get-error))
     nil))
 
 
 (defun work ()
  ;; (declare (optimize (speed 3) (safety 0) (debug 0)))
   (with:all
-      ((vec (rgb-back '(0.0 0.0 0.0 1.0)))		;'(0.9 0.2 0.3 1.0)
-       (vec (rgb-fill '(1.0 1.0 1.0 1.0))))
+      ((vec (rgb-back '(0.0 0.0 0.0 1.0)))	
+       (vec (rgb-fill '(1.0 1.0 0.0 1.0)))
+       (vec (rgb-stroke	'(0.9 0.2 0.3 1.0)
+	     )))
     (background rgb-back)
     (set-fill rgb-fill)
+    (set-stroke rgb-stroke)
     ;;    (vg:set-i vg:rendering-quality vg:quality-faster)
     (stroke-width 1.0)
     (circle 10.0 10.0 3.0)
